f3b697f2c826f0205bee7915ce7e0d8a6a141f75
roshnitriveni <61348743+roshnitriveni@users.noreply.github.com>
2020-05-31 18:22:42 +0530

    First commit

---
+++ FolderCopy-ThreadJob.ps1
@@ -0,0 +1,32 @@
+﻿#Add files/folders to sourcefolder so that will be copied in dest folder
+$origin=$PSScriptRoot
+Write-Host $origin
+function sync(){
+$source = $origin +"\Source"  
+$target = $origin +"\Dest\A"   
+$target2 = $origin +"\Dest\B"   
+Copy-Item -Path $source\* -Destination $target -Recurse   -Force
+Copy-Item -Path $source\* -Destination $target2 -Recurse   -Force
+}
+
+
+function async(){
+$source = $origin+"\Source"  
+$target = $origin+"\Dest\A"   
+$copyJob1 = Start-ThreadJob –ScriptBlock {  
+   
+    Copy-Item -Path $using:source\* -Destination $using:target -Recurse   -Force
+}
+#Code here will run async  
+$target2 = $origin+ "\Dest\B"   
+$copyJob2 = Start-ThreadJob –ScriptBlock {  
+   
+    Copy-Item -Path $using:source\* -Destination $using:target2 -Recurse   -Force
+}
+#Code here will run async  
+Wait-Job $copyJob1  
+Wait-Job $copyJob2  
+}
+Measure-Command{ sync}
+Measure-Command{ async}
+#Get-Job|Remove-Job
